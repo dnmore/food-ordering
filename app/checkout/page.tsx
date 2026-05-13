bf830a9bbe2fd640e129c1d6a55c172067d1c36a
dnmore <morettidn@gmail.com>
2026-05-13 11:36:08 +0200

    add checkout summary table

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,28 +1,61 @@
 "use client"
 import { useContext } from "react"
 import CartContext from "../context/cart-context"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function Page() {
   const { cartItems, totalPrice } = useContext(CartContext)
 
   return (
-    <div className="container mx-auto">
-      <div className="flex-col items-center gap-4 py-10">
-        <h1 className="mb-6 text-center text-3xl font-extrabold md:text-5xl">
+    <div className="container mx-auto px-4">
+      <div className="flex-col items-center gap-4 py-10 px-2">
+        <h1 className="mb-6 text-3xl font-extrabold md:text-5xl">
           Checkout
         </h1>
-        <ul className="flex flex-col max-w-sm mx-auto gap-4 p-4">
-          {cartItems.map((item) => (
-            <li key={item.id} className="flex justify-between items-center border p-4 rounded-lg">
-              {item.quantity} x {item.name} - €
-              {(item.price * item.quantity).toFixed(2)}
-            </li>
-          ))}
-        </ul>
-        <div className="mt-4 p-4 text-2xl font-bold text-center">
-          Total: €{totalPrice.toFixed(2)}
+        
+        <Table className="w-full max-w-2xl">
+      <TableCaption>Summary of selected products</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-25">Product</TableHead>
+          <TableHead className="text-center">Quantity</TableHead>
+          <TableHead className="text-right">Price</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {cartItems.map((item) => (
+          <TableRow key={item.id}>
+            <TableCell>{item.name}</TableCell>
+            <TableCell className="text-center">{item.quantity}</TableCell>
+            
+            <TableCell className="text-right">{(item.price * item.quantity).toLocaleString("it-IT", { style: "currency", currency: "EUR" })}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">{totalPrice.toLocaleString("it-IT", { style: "currency", currency: "EUR" })}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  
+          
+           
+          
         </div>
+
+       
       </div>
-    </div>
+    
   )
 }
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   return (
     <div className="container mx-auto flex items-center">
       <div className="flex-col items-center gap-4 py-10">
-        <h1 className="mb-6 text-center text-3xl font-extrabold md:text-5xl">
+        <h1 className="mb-6 pl-4 text-3xl font-extrabold md:text-5xl">
           Menu
         </h1>
         <ul className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 p-4">
