6be7df408bb6960cb39f1326b280038e052d1f2a
dnmore <morettidn@gmail.com>
2026-07-13 18:48:50 +0200

    update menu sidebar

--- app/(admin)/dashboard/orders/[id]/details/page.tsx
+++ app/(admin)/dashboard/orders/[id]/details/page.tsx
@@ -1,17 +1,18 @@
-import type { Metadata} from "next"
+import type { Metadata } from "next"
 import { Suspense } from "react"
 import { getOrderDetails } from "@/lib/data"
-import {OrderTable} from "@/components/table/order-table"
+import { OrderTable } from "@/components/table/order-table"
 import { notFound } from "next/navigation"
 import { SkeletonTable } from "@/components/layout/skeletons"
 
-export const metadata: Metadata = {
-  title: 'Order Details',
-};
 
+export const metadata: Metadata = {
+  title: "Order Details",
+}
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params
+
   const orderDetails = await getOrderDetails(id)
 
   if (!orderDetails) {
@@ -20,14 +21,15 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 
   return (
     <div className="pt-6">
-      <h1 className="mb-2 ml-1 text-xl font-semibold md:text-2xl">Order Details</h1>
+      <h1 className="mb-2 ml-1 text-xl font-semibold md:text-2xl">
+        Order Details
+      </h1>
       <div className="container max-w-3xl py-10">
-        <Suspense fallback={<SkeletonTable/>}>
+        <Suspense fallback={<SkeletonTable />}>
           <OrderTable order={orderDetails} />
         </Suspense>
-      
+        
       </div>
-       
     </div>
   )
 }
--- components/layout/menu-sidebar.tsx
+++ components/layout/menu-sidebar.tsx
@@ -1,20 +1,15 @@
 "use client"
 
 import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { clsx } from "clsx"
-
 import { CategorySelectOption } from "@/lib/definitions"
-
+import { Button } from "@/components/ui/button"
 
 type MenuSidebarProps = {
   categories: CategorySelectOption[]
 }
 
 export function MenuSidebar({ categories }: MenuSidebarProps) {
-  const pathname = usePathname()
-  
-  
+
 
   return (
     <aside
@@ -24,26 +19,26 @@ export function MenuSidebar({ categories }: MenuSidebarProps) {
       <nav
         className="flex flex-1 flex-col gap-2 p-2"
         aria-label="Menu navigation"
-      > <Link href="/menu" className="flex h-10 uppercase items-center rounded-md bg-background text-sm text-foreground transition-colors hover:bg-muted mb-4 md:px-3">Menu</Link>
-      <p className="text-muted-foreground text-xs uppercase">Categories</p>
-        {categories.map((category) => (
-          <Link
-            key={category.id}
-            href={`/menu/categories/${category.title}`}
-            aria-label={category.title}
-            aria-current={pathname === `/menu/categories/${category.title}` ? "page" : undefined}
-            className={clsx(
-              "group flex h-10 items-center justify-center gap-3 rounded-md bg-background text-sm text-foreground transition-colors hover:bg-muted md:justify-start md:px-3",
-              {
-                "bg-muted font-semibold": pathname === `/menu/categories/${category.title}`,
-              }
-            )}
-          >
-          
-
-            {category.title}
-          </Link>
-        ))}
+      >
+        <Button asChild variant="link">
+          <Link href="/">Home</Link>
+        </Button>
+        
+        <div className="mt-4 flex flex-col gap-2 border-t pt-4">
+         <Button asChild >
+          <Link href="/menu">Menu</Link>
+        </Button>
+          {categories.map((category) => (
+            <Button asChild variant="link" key={category.id}>
+              <Link
+                href={`/menu/categories/${category.title}`}
+                aria-label={category.title}
+              >
+                {category.title}
+              </Link>
+            </Button>
+          ))}
+        </div>
       </nav>
     </aside>
   )
